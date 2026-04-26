7be523e77a479ec9ae6273466187ddcfafbbfc7f
Tonny <tonny@MacBook-Air-van-Tonny.local>
2026-04-26 17:32:18 +0200

    Use local hero landmark images

--- inc/theme-core.php
+++ inc/theme-core.php
@@ -65,7 +65,7 @@ function wc_enqueue_assets() {
         'https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&family=DM+Sans:opsz,wght@9..40,400;9..40,500;9..40,600;9..40,700;9..40,800;9..40,900&display=swap',
         [], null
     );
-    wp_enqueue_style('wc-main', get_template_directory_uri() . '/assets/css/main.css', ['wc-fonts'], '4.9');
+    wp_enqueue_style('wc-main', get_template_directory_uri() . '/assets/css/main.css', ['wc-fonts'], '5.0');
     wp_enqueue_script('wc-main', get_template_directory_uri() . '/assets/js/main.js', [], '4.7', true);
     wp_localize_script('wc-main', 'wcVars', array(
         'ajaxUrl' => admin_url('admin-ajax.php'),
